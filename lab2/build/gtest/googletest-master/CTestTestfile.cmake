0c9827f034b916bbd6217742c987fedef34bd605
bloodthirstypiggy <piggybloodthirsty@gmail.com>
2021-10-03 09:35:03 +0300

    tests with getters and cmakelist and no buildings

--- lab2/build/gtest/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/piggy/Desktop/cpplabs/lab2/gtest/googletest-master
-# Build directory: /home/piggy/Desktop/cpplabs/lab2/build/gtest/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- lab2/build/gtest/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/piggy/Desktop/cpplabs/lab2/gtest/googletest-master/googletest
-# Build directory: /home/piggy/Desktop/cpplabs/lab2/build/gtest/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
