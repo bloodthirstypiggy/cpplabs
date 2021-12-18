a15a1f2e30ac2c313efad9fdbc303b417f93ed89
bloodthirstypiggy <bloodthirstypiggy@gmail.com>
2021-12-18 12:37:35 +0000

    lab4 start no graph

--- lab4/tdCells/cell.hpp
+++ lab4/tdCells/cell.hpp
@@ -14,33 +14,33 @@ protected:
     std::pair<int, int> coordinate;
     std::vector<Enemy::Enemies *> en;
     wall::Wall *defend;
-    int distance_from_castle;
-    int distance_for_plane;
+    int distance_from_magic;
+    int distance_for_magic;
 public:
     Cell() {
         coordinate = std::make_pair(0, 0);
-        distance_from_castle = -1;
-        distance_for_plane = -1;
+        distance_from_magic = -1;
+        distance_for_magic = -1;
         defend = nullptr;
     }
 
     Cell(int x, int y) {
         coordinate = std::make_pair(x, y);
-        distance_from_castle = -1;
+        distance_from_magic = -1;
         distance_for_plane = -1;
         defend = nullptr;
     }
     Cell(int x, int y, int t) {
         if(t==4){
             coordinate = std::make_pair(x, y);
-            distance_from_castle = 0;
+            distance_from_magic = 0;
             distance_for_plane = 0;
             defend = nullptr;
             type = t;
         }
         else {
             coordinate = std::make_pair(x, y);
-            distance_from_castle = -1;
+            distance_from_magic = -1;
             distance_for_plane = -1;
             defend = nullptr;
             type = t;
@@ -64,7 +64,7 @@ public:
     }
 
     [[nodiscard]] int get_distance() const {
-        return distance_from_castle;
+        return distance_from_magic;
     }
 
     [[nodiscard]] int get_distance_for_magic() const {
@@ -76,12 +76,12 @@ public:
     }
 
     bool change_distance(int x) {
-        if (distance_from_castle == -1) {
-            distance_from_castle = x + 1;
+        if (distance_from_magic == -1) {
+            distance_from_magic = x + 1;
             return true;
         } else {
-            if ((distance_from_castle > (x + 1))) {
-                distance_from_castle = x + 1;
+            if ((distance_from_magic > (x + 1))) {
+                distance_from_magic = x + 1;
                 return true;
             } else {
                 return false;
