b18c8a3cd19096381aecb1e10b41d392ec6a864c
bloodthirstypiggy <bloodthirstypiggy@gmail.com>
2021-12-25 12:27:35 +0000

    headers

---
+++ lab4/headers/main.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Map
+{
+	float h, w, wh = 16.0f/9.0f;
+	int secCounter = 0;
+	float mapFromX = -16.0f / 9.0f;
+	float mapFromY = -0.5f;
+	float mapToY = 1;
+	float mapToX = 16.0f / 9.0f;
+	float sizeOfSquare;
+}
--- lab4/tdEnemies/hero.cpp
+++
@@ -1,8 +0,0 @@
-#include "hero.hpp"
-
-void Hero::give_aura(std::vector<Enemies> v) {
-    std::vector<Enemies>::iterator i;
-    for (i = v.begin(); i != v.end(); i++) {
-
-    }
-}
